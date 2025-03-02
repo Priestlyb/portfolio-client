1077ac485c0c82a0a016ba4673381a22f3f79315
Priestly Patrick bassey <pbassey30@gmail.com>
2025-03-02 15:23:07 +0100

    made Changes to the navbar

--- src/config.js
+++ src/config.js
@@ -3,3 +3,6 @@ import axios from "axios"
 export const axiosInstance = axios.create({
     baseURL: "https://personalsite-7yf035rs.b4a.run/"
 })
+
+// https://personalsite-7yf035rs.b4a.run/ 
+// http://localhost:3005
