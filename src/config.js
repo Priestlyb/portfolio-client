b962f51b1af58f9cd79452a473e66ca81f012c10
Priestly Patrick bassey <pbassey30@gmail.com>
2025-04-23 07:35:58 +0100

    modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -1,23 +1,9 @@
 import axios from "axios"
 
-let baseURL;
-
-switch (window.location.hostname) {
-  case "localhost":
-  case "127.0.0.1":
-    baseURL = "http://localhost:3005";
-    break;
-
-  default:
-    baseURL = "https://personalsite-7yf035rs.b4a.run/";
-    break;
-}
-
 export const axiosInstance = axios.create({
-  baseURL,
-  withCredentials: true,
-});
-
+    baseURL: "https://personalsite-7yf035rs.b4a.run/",
+    withCredentials: true, // Important for sessions
+})
 
 // https://personalsite-7yf035rs.b4a.run/ 
 // http://localhost:3005
