54378faba81a35e1279b2f0e2593689c79999196
Priestly Patrick bassey <pbassey30@gmail.com>
2025-09-19 18:13:41 +0100

    mistakenly changed the baseURL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3005",
+    baseURL: "https://portfolio-api-yey0.onrender.com/",
     withCredentials: true,
 })
 
